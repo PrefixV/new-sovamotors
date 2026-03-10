dc15e3d193c6bef80379838a3d80e04e0cb75636
Grisha Chapurin <grishachapurin@gmail.com>
2026-03-10 09:52:56 +0300

    Add pricelist

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ document.querySelector(".socials-container-trigger")
 
 document.querySelector(".services-container__row-card__show__button")
     .addEventListener("click", toggleServices);
+
+const accordionContainer = document.querySelector('.pricelist-container__accordion');
+accordionContainer.addEventListener('click', (e) => {
+    const accordionItem = e.target.closest('.pricelist-container__accordion-item');
+    if (accordionItem) {
+        const accordionItemDescription = accordionItem.querySelector('.pricelist-container__accordion-item__description');
+        const accordionItemArrow = accordionItem.querySelector('svg');
+        if (accordionItemDescription) {
+            accordionItemDescription.classList.toggle('pricelist-container__accordion-item__description--hidden');
+            accordionItemArrow.classList.toggle('rotate-arrow');
+        }
+    }
+})
